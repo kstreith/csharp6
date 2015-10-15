ddd6f9920221e5dde92c337e8b3cd47b222e6784
Kip Streithorst <kstreith@yahoo.com>
2015-10-15 00:12:13 -0400

    WPF sample

---
+++ nameofOperator/NotifySampleViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameof
+{
+    class NotifySampleViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+                    }
+                }
+            }
+        }
+
+        private double _orderAmount;
+        public double OrderAmount
+        {
+            get { return _orderAmount; }
+            set
+            {
+                if (value != _orderAmount)
+                {
+                    _orderAmount = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("OrderAmount"));
+                    }
+                }
+            }
+        }
+
+    }
+
+    #region CSharp6 Working Code
+    //class NotifySampleViewModelCSharp6 : INotifyPropertyChanged
+    //{
+    //    public event PropertyChangedEventHandler PropertyChanged;
+
+    //    private string _title;
+    //    public string Title
+    //    {
+    //        get { return _title; }
+    //        set
+    //        {
+    //            if (value != _title)
+    //            {
+    //                _title = value;
+    //                if (PropertyChanged != null)
+    //                {
+    //                    PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //    private double _orderAmount;
+    //    public double OrderAmount
+    //    {
+    //        get { return _orderAmount; }
+    //        set
+    //        {
+    //            if (value != _orderAmount)
+    //            {
+    //                _orderAmount = value;
+    //                if (PropertyChanged != null)
+    //                {
+    //                    PropertyChanged(this, new PropertyChangedEventArgs("OrderAmount"));
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //}
+    #endregion
+
+}
